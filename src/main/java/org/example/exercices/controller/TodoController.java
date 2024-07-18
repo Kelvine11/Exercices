02cf4061431e9073de79ffe52aba93e6def7fe99
Kelvine-Palet <kelvine1.palet@gmail.com>
2024-07-18 16:00:40 +0200

    First commit

---
+++ src/main/java/org/example/exercices/controller/TodoController.java
@@ -0,0 +1,43 @@
+package org.example.exercices.controller;
+
+import org.example.exercices.entity.Todo;
+import org.example.exercices.service.TodoService;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+
+    }
+
+    @RequestMapping("/todo/{id}")
+    public String showTodo(@PathVariable int id, Model model)
+    {
+       model.addAttribute("todo", this.todoService.showTodo(id) );
+       return "todo/show";
+    }
+
+    @RequestMapping("/todos")
+    public String listTodo(Model model) {
+        model.addAttribute("todos", this.todoService.getTodos());
+        return "todo/list";
+    }
+
+    @RequestMapping("/listJson")
+    @ResponseBody
+    public List<Todo> getTodo(){
+        return this.todoService.getTodos();
+    }
+
+}
---
+++ src/main/java/org/example/exercices/service/TodoService.java
@@ -0,0 +1,32 @@
+package org.example.exercices.service;
+
+import org.example.exercices.entity.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private List<Todo> todos = new ArrayList<Todo>();
+
+    public TodoService() {
+        todos.add(new Todo(1,"toto", "un toto", true));
+        todos.add(new Todo(2,"tata", "un tata", false));
+    }
+
+    public Todo showTodo(int id) {
+        for (Todo todo : todos) {
+            if (todo.getId() == id) {
+                return todo;
+            }
+        }
+        return null;
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+}
